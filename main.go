356790809a3d89a6415b85e42aa3deb18db69d14
Mathus Ferreira <matheus.ferreira165@outlook.com>
2023-08-01 21:51:00 -0300

    Create func main for test methods of hash_table

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Pessoa struct {
 	Nome      string
 	Sobrenome string
@@ -8,5 +10,30 @@ type Pessoa struct {
 }
 
 func main() {
+	pessoas := []Pessoa{
+		{"Matheus", "Ferreira", 24, "Masculino"},
+		{"Maria", "Ferreira", 24, "Feminino"},
+		{"Joao", "Ferreira", 24, "Masculino"},
+		{"Tereza", "Ferreira", 24, "Feminino"},
+	}
 
+	table := HashTable{}
+
+	keys := make([]int, len(pessoas))
+
+	for i, pessoa := range pessoas {
+		keys[i] = table.Put(pessoa)
+	}
+
+	for _, key := range keys {
+		ps := table.Get(key)
+		for _, p := range ps {
+			fmt.Println(p.Nome, p.Sobrenome, p.Idade, p.Sexo)
+		}
+	}
+
+	table.Remove("Maria")
+
+	tiago := table.Search("Tereza")
+	fmt.Println(tiago)
 }
